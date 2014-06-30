4640c755adb273e81f14b78d06d81f15e823b6cd
Mike Heavers <mheavers@gmail.com>
2014-06-30 18:03:24 -0400

    complete, responsive first page, no transition effects

--- js/main.js
+++ js/main.js
@@ -1,1 +1,42 @@
+var mainModule = (function($, window) {
+
+  configVars = {
+    scrollThreshold: 250
+  }
+
+  init = function() {
+    addListeners();
+  }
+
+  addListeners = function(){
+    $(document).on('scroll',onWindowScroll);
+    $('.article-option.subnav').on('click',onArticleOptionClick);
+  }
+
+  onWindowScroll = function(e){
+    var scrollTop = $(window).scrollTop();
+    var scrollPos = Math.round((scrollTop / configVars.scrollThreshold) * 100);
+    if (scrollPos > 100){
+        //offWindowScroll();
+    } else {
+        $('.content-bg-image-container').css({'background-position-y' : scrollPos + '%'});
+    }
+
+    console.log(scrollPos);
+  }
+
+  offWindowScroll = function(){
+    $(document).off('scroll',onWindowScroll);
+  }
+
+  onArticleOptionClick = function(){
+    $(this).toggleClass('active');
+  }
+
+
+  return {
+    init: init,
+  };
+
+})(jQuery, window);
 
