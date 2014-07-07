4f1d9ee9fcc83d0370603b5e790977eb24d8a94f
Mike Heavers <mheavers@gmail.com>
2014-07-07 14:21:02 -0400

    remove article sidebar and home page images. Increase bg ing height, add fixed email container,

--- js/main.js
+++ js/main.js
@@ -1,11 +1,34 @@
 var mainModule = (function($, window) {
 
+  configVars = {
+    scrollThreshold: 350
+  }
+
   init = function() {
     addListeners();
   }
 
   addListeners = function(){
+    $(document).on('scroll',onWindowScroll);
     $('.article-option.subnav').on('click',onArticleOptionClick);
+  }
+
+  onWindowScroll = function(e){
+    var scrollTop = $(window).scrollTop();
+    var scrollThreshold = configVars.scrollThreshold;
+    if (scrollTop > scrollThreshold){
+        showBottomEmail();
+    } else {
+        hideBottomEmail();
+    }
+  }
+
+  showBottomEmail = function(){
+    $('#fixed-email-container:not(.active)').addClass('active');
+  }
+
+  hideBottomEmail = function(){
+    $('#fixed-email-container').removeClass('active');
   }
 
   onArticleOptionClick = function(){
